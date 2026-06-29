858a8e73ff6ddd0165e3d1ea028f944452665cf2
annsayuri <annsayu12@gmail.com>
2026-06-30 01:47:42 +0530

    Modifed view folder

--- lib/features/expenses/Views/add_transaction_screen.dart
+++ lib/features/expenses/Views/add_transaction_screen.dart
@@ -8,6 +8,16 @@ class AddTransactionScreen extends StatefulWidget {
 }
 
 class _AddTransactionScreenState extends State<AddTransactionScreen> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  String _selectedType = 'Income'; // Default එක විදිහට Income තියෙන්නේ
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,12 +36,14 @@ class _AddTransactionScreenState extends State<AddTransactionScreen> {
                 labelText: 'Title',
                 border: OutlineInputBorder(),
               ),
+              controller: _titleController,
             ),
             const SizedBox(height: 16.0), // Fields දෙක අතර පරතරය ↕️
             
             // Amount Input Field
             TextFormField(
               keyboardType: TextInputType.number,
+              controller: _amountController,
               decoration: const InputDecoration(
                 labelText: 'Amount',
                 border: OutlineInputBorder(),
