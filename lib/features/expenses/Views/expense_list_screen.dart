b79b8868d72f7b186db0f4263a55f21031bb4f52
annsayuri <annsayu12@gmail.com>
2026-07-12 19:05:43 +0530

    Updated

--- lib/features/expenses/Views/expense_list_screen.dart
+++ lib/features/expenses/Views/expense_list_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart'; 
 import 'package:fl_chart/fl_chart.dart';
-import 'analytics_screen.dart'; 
+import '../../analytics_screen.dart';
 
 import '../../../core/database/db_helper.dart'; 
 import 'add_transaction_screen.dart';
