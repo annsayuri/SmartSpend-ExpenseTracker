9974467fde39e973a71d1b351ccccbfeecd0c499
Sayuri Kotikawaththa <annsayu12@gmail.com>
2026-08-04 23:50:06 +0530

    Update expense_provider.dart

--- lib/features/expenses/provider/expense_provider.dart
+++ lib/features/expenses/provider/expense_provider.dart
@@ -1,80 +1,21 @@
-import 'package:flutter/material.dart';
-import 'package:smartspend_expensetracker/features/expenses/model/expense_model.dart';
-import 'package:smartspend_expensetracker/features/expenses/repository/expense_repository.dart';
+enum TransactionType { income, expense }
 
-class ExpenseProvider extends ChangeNotifier {
-  final ExpenseRepository _repository = ExpenseRepository();
+enum ExpenseCategory { food, transport, entertainment, bills, education, salary, savings, others }
 
-  List<ExpenseModel> _transactions = [];
-  double _budgetLimit = 0.0;
-  bool _isLoading = false;
+class TransactionModel {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  final ExpenseCategory category;
+  final TransactionType type;
 
-  // Getters
-  List<ExpenseModel> get transactions => _transactions;
-  double get budgetLimit => _budgetLimit;
-  bool get isLoading => _isLoading;
-
-  // Calculated Properties
-  double get totalIncome => _transactions
-      .where((item) => item.type == TransactionType.income)
-      .fold(0.0, (sum, item) => sum + item.amount);
-
-  double get totalExpenses => _transactions
-      .where((item) => item.type == TransactionType.expense)
-      .fold(0.0, (sum, item) => sum + item.amount);
-
-  double get totalBalance => totalIncome - totalExpenses;
-
-  // 1. Initial Data Loading
-  Future<void> loadInitialData() async {
-    _isLoading = true;
-    notifyListeners();
-
-    try {
-      _transactions = await _repository.fetchTransactions();
-      _budgetLimit = await _repository.fetchBudget();
-    } catch (e) {
-      debugPrint('Error loading data: $e');
-    } finally {
-      _isLoading = false;
-      notifyListeners();
-    }
-  }
-
-  // 2. Add Expense / Income
-  Future<bool> addTransaction(ExpenseModel expense) async {
-    final result = await _repository.addTransaction(expense);
-    if (result > 0) {
-      await loadInitialData();
-      return true;
-    }
-    return false;
-  }
-
-  // 3. Update Transaction
-  Future<bool> updateTransaction(ExpenseModel expense) async {
-    final result = await _repository.updateTransaction(expense);
-    if (result > 0) {
-      await loadInitialData();
-      return true;
-    }
-    return false;
-  }
-
-  // 4. Delete Transaction
-  Future<bool> deleteTransaction(int id) async {
-    final result = await _repository.deleteTransaction(id);
-    if (result > 0) {
-      await loadInitialData();
-      return true;
-    }
-    return false;
-  }
-
-  // 5. Update Budget Limit
-  Future<void> setBudget(double amount) async {
-    await _repository.saveBudget(amount);
-    _budgetLimit = amount;
-    notifyListeners();
-  }
+  TransactionModel({
+    required this.id,
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+    required this.type,
+  });
 }
