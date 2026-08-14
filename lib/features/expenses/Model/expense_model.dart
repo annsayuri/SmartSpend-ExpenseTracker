7925c3539542c0f685ad0d34b273a06f5ba8dd15
annsayuri <annsayu12@gmail.com>
2026-08-14 11:16:13 +0530

    Match expense model and expense repository

--- lib/features/expenses/Model/expense_model.dart
+++ lib/features/expenses/Model/expense_model.dart
@@ -1,22 +1,51 @@
-
 enum TransactionType { income, expense }
 
 enum ExpenseCategory { food, transport, entertainment, bills, education, salary, savings, others }
 
-class TransactionModel {
-  final String id;
+class ExpenseModel {
+  final int? id; // Database id එක සදහා
   final String title;
   final double amount;
   final DateTime date;
   final ExpenseCategory category;
   final TransactionType type;
 
-  TransactionModel({
-    required this.id,
+  ExpenseModel({
+    this.id,
     required this.title,
     required this.amount,
     required this.date,
     required this.category,
     required this.type,
   });
+
+  // Database එකට දාන්න Map එකකට Convert කිරීම
+  Map<String, dynamic> toMap() {
+    return {
+      if (id != null) 'id': id,
+      'title': title,
+      'amount': amount,
+      'date': date.toIso8601String(),
+      'category': category.name,
+      'type': type.name,
+    };
+  }
+
+  // Database එකෙන් ගන්න Map එක Object එකක් කිරීම
+  factory ExpenseModel.fromMap(Map<String, dynamic> map) {
+    return ExpenseModel(
+      id: map['id'],
+      title: map['title'] ?? '',
+      amount: (map['amount'] as num).toDouble(),
+      date: DateTime.parse(map['date']),
+      category: ExpenseCategory.values.firstWhere(
+        (e) => e.name == map['category'],
+        orElse: () => ExpenseCategory.others,
+      ),
+      type: TransactionType.values.firstWhere(
+        (e) => e.name == map['type'],
+        orElse: () => TransactionType.expense,
+      ),
+    );
+  }
 }
